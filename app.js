5c4ecb6a2e277f44736a1ae9cd75c6b509bc0ea6
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-12-08 14:28:26 +0400

    Changes

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ let myTip = document.querySelector("#tip-amount");
 let myTotal = document.querySelector("#total-per-person");
 let myBill = document.querySelector("#bill-amount");
 let myNumber =  document.querySelector("#number-of-people");
-let percent = document.querySelector('input[name="tip"]:checked');
 let calculate =  document.querySelector("#calculate");
 
 let myFunction = ()=> {
+    let percent = document.querySelector('input[name="tip"]:checked');
+
     let bill = parseFloat(myBill.value);
     let number = parseFloat(myNumber.value);
     let per = parseFloat(percent.value);
